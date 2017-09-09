474751b75b3dc3f0e87fa6035b363e6d362de5a6
ChengShen1996 <617984532@qq.com>
2017-09-08 21:30:35 -0700

    from Cheng's macbook

--- app.rb
+++ app.rb
@@ -40,7 +40,25 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    redirect '/show'
+    # ---------------------
+    if letter.nil? || letter.match(/[^a-zA-Z]+/)
+      flash[:message] = "Invalid guess."
+      redirect '/show'
+    elsif !@game.guess(letter)
+      flash[:message]="You have already used that letter."
+      redirect '/show'
+    else
+      if @game.check_win_or_lose == :win
+        redirect '/win'
+      elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+      else
+        redirect '/show'
+      end
+    end
+
+
+    #redirect '/show'
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -50,17 +68,31 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
     erb :show # You may change/remove this line
+  end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,10 +7,60 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  #----------------------my code is below
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @current_status = :play
   end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess(the_letter)
+    if the_letter.nil? || the_letter.empty? || the_letter =~/[^a-zA-Z]+/
+      raise ArgumentError
+    end
+  
+  the_letter=the_letter.downcase
+  if (!@guesses.include? the_letter) && (!@wrong_guesses.include? the_letter)
+      if @word.include? the_letter
+        @guesses += the_letter
+        if !word_with_guesses.include?('-') && @current_status != :lose
+          @current_status = :win
+        end
+          return true
+      else
+        @wrong_guesses += the_letter
+        if @wrong_guesses.length >=7
+          @current_status = :lose
+        end
+        return true
+      end
+    end
+    return false
+    
+  end
+
+  def word_with_guesses
+    temp=''
+    @word.split('').each do |c|
+      if @guesses.include? c
+        temp +=c
+      else
+        temp +='-'
+      end
+    end
+  return temp
+  end
+  
+  def check_win_or_lose
+    return @current_status
+  end
+
+#----------------------------------------------------------------------
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
